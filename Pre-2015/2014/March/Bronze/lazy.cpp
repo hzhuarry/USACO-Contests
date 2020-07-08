7abfba65c08ebec570fc74f1cffefa53e60d545b
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-07-07 21:16:30 -0400

    Create lazy.cpp

---
+++ Pre-2015/2014/March/Bronze/lazy.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <fstream>
+
+using namespace std;
+
+#define endl "\n"
+#define sz(x) (int)x.size()
+#define pf push_front
+#define pb(s) push_back(s)
+#define mp(a,b) make_pair(a,b)
+#define f first
+#define s second
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PI 3.14159265358979323846264338327950L
+#define lb lower_bound
+#define ub upper_bound
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<str> vs;
+typedef vector<ld> vd;
+typedef vector<str> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpd;
+
+const int MOD = 1000000007;
+const ll INF=1e18;
+const int dx[4]= {-1,1,0,0}, dy[4]= {0,0,-1,1};
+
+int N,K,ans;
+vpii grass;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout<<fixed<<setprecision(12);
+    ifstream cin ("lazy.in");
+    ofstream cout ("lazy.out");
+    
+    cin>>N>>K;
+    for(int i=0;i<N;++i){
+        int g_i,pos;
+        cin>>g_i>>pos;
+        grass.pb(mp(pos,g_i));
+    }
+    SORT(grass);
+    //sliding window
+    int start=0,sum=0;
+    for(int i=0;i<N;++i){
+        sum+=grass[i].s;
+        while(grass[i].f-grass[start].f>2*K){
+            sum-=grass[start].s;
+            start++;
+        }
+        ans=max(ans,sum);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
