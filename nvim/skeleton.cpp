ca7617a01402feac3df752eba71550eda6002b40
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-07-13 20:02:05 -0400

    Update skeleton.cpp

--- nvim/skeleton.cpp
+++ nvim/skeleton.cpp
@@ -125,6 +125,37 @@ template<class H, class... T> void print(const H& h, const T&... t) {
     print(t...);
 }
 
+struct DSU{
+    vi parent,size;
+    void init(int N){
+        parent.resize(N),size.resize(N);
+        for(int i=0;i<N;++i){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    int find_set(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=find_set(parent[x]);
+    }
+    int set_size(int x){
+        return size[find_set(x)];
+    }
+    bool same_set(int a,int b){
+        return find_set(a)==find_set(b);
+    }
+    void merge(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            if (size[a] < size[b])
+                swap(a, b);
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+};
+
 void fileIO(string filename) {
     freopen((filename+".in").c_str(), "r", stdin);
     freopen((filename+".out").c_str(), "w", stdout);
