81da54762f48a93fc4871f8558b38b57773dd6ba
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-07-13 23:17:38 -0400

    Update skeleton.cpp

--- nvim/skeleton.cpp
+++ nvim/skeleton.cpp
@@ -59,37 +59,6 @@ template<class T> bool ckmax(T& a, const T& b) {
     return a < b ? a = b, 1 : 0; } 
 
 
-struct DSU{
-    vi parent,size;
-    void init(int N){
-        parent.resize(N),size.resize(N);
-        for(int i=0;i<N;++i){
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-    int find_set(int x){
-        if(parent[x]==x) return x;
-        return parent[x]=find_set(parent[x]);
-    }
-    int set_size(int x){
-        return size[find_set(x)];
-    }
-    bool same_set(int a,int b){
-        return find_set(a)==find_set(b);
-    }
-    void merge(int a, int b) {
-        a = find_set(a);
-        b = find_set(b);
-        if (a != b) {
-            if (size[a] < size[b])
-                swap(a, b);
-            parent[b] = a;
-            size[a] += size[b];
-        }
-    }
-};
-
 void fileIO(string filename) {
     freopen((filename+".in").c_str(), "r", stdin);
     freopen((filename+".out").c_str(), "w", stdout);
